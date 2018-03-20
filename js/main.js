5342166043aca69127e00c79f37d57087f328adf
jonatascp <jonatascp@gmail.com>
2018-03-19 22:32:46 -0300

    Add process component

--- js/main.js
+++ js/main.js
@@ -14,11 +14,14 @@ function drop(ev) {
     console.log(element);
     console.log(element.name);
     var nameImage = element.name;
-    if (nameImage = 'images/database-icon.png') {
-    	create_component('images/\database-icon.png', 'component_database');
-    } 
     if (nameImage = 'images/server.png') {
-    	create_component('images/\server.png', 'component_server');
+    	create_component('images/\server.png', 'component-server');
+    }
+    if (nameImage = 'images/database-icon.png') {
+    	create_component('images/\database-icon.png', 'component-database');
+    } 
+    if (nameImage = 'images/process-icon.png') {
+    	create_component('images/\process-icon.png', 'component-process');
     }
 }
 
