6d74832ff3673eb4061bc8f81ad2beceef79dde5
jonatascp <jonatascp@gmail.com>
2018-03-19 22:25:17 -0300

    create images dinamic

--- js/main.js
+++ js/main.js
@@ -10,4 +10,19 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    var element = document.getElementById(data);
+    console.log(element);
+    console.log(element.name);
+    var nameImage = element.name;
+    if (nameImage = 'images/database-icon.png') {
+    	create_component('images/\database-icon.png', 'component_database');
+    } 
+    if (nameImage = 'images/server.png') {
+    	create_component('images/\server.png', 'component_server');
+    }
+}
+
+function create_component(srcImage, div) {
+    var idImage = new Date().getTime();
+    document.getElementById(div).innerHTML = "<img src=" + srcImage + " id=" + idImage + " name=" + srcImage + " draggable='true' ondragstart='drag(event)' width='50' height='50'></img>"
 }
