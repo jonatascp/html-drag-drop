96b0bfe638ca657a20ad29c864e2e930e28e2929
jonatascp <jonatascp@gmail.com>
2018-03-21 21:53:29 -0300

    Criando a tabela dinamica

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
-document.addEventListener('DOMContentLoaded', function(){ 
-        create_component('images/\server.png', 'component-server');
-        create_component('images/\database-icon.png', 'component-database');
-        create_component('images/\process-icon.png', 'component-process');
-    }, false);
+$(document ).ready(function() {
+	create_component('images/\server.png', 'component-server');
+    create_component('images/\database-icon.png', 'component-database');
+    create_component('images/\process-icon.png', 'component-process');
+    var qtdLinha = 6;
+    var qtdColuna = 10;
+    for(i=0; i<qtdLinha; i++) {
+    	$("#table-area").append("<tr>");
+    	for(j=0; j<qtdColuna; j++) {
+    		$("#table-area tr:last").append("<td><div id='div" + i +"_" + j + "' ondrop='drop(event)' ondragover='allowDrop(event)'></div></td>");
+    	}
+    }
+});
